b3dc10d8b7b56c72683e0c0a4df4cc222a38c1b8
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-04 14:44:47 +0000

    chore: update allowed origins for CORS configuration to support production environment

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,36 @@
+// // Allowed origins array
+// const allowedOrigins = [
+//   `http://${ORIGIN_ADDRESS}:${ORIGIN_PORT}`,
+//   'https://studio.apollographql.com',
+// ];
+
+// // Express
+// const app = express();
+
+// // For Express routes (REST, GraphQL, etc.)
+// import cors from 'cors';
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true,
+// }));
+
+// // For socket.io
+// const httpServer = createServer(app);
+// const io = new Server(httpServer, { 
+//   cors: { 
+//     origin: allowedOrigins,
+//     credentials: true,
+//   }
+// });
+
+
 // Origin.
-export const ORIGIN_ADDRESS = "localhost";
-export const ORIGIN_PORT = 4200;
+// export const ORIGIN_ADDRESS = "localhost";
+// export const ORIGIN_PORT = 4200;
+export const ORIGIN_ADDRESS = "studio.apollographql.com";
+export const ORIGIN_PORT = 443;
+
+export const ORIGIN = `https://${ORIGIN_ADDRESS}:${ORIGIN_PORT}`;
 
 // Server.
 export const ADDRESS = "localhost";
