40ba6f486eac95bd26b99bcac49fd9e6cd798432
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-04 14:44:16 +0000

    chore: make users query function asynchronous for improved performance

--- src/graphql/users/users.query.resolver.ts
+++ src/graphql/users/users.query.resolver.ts
@@ -1,7 +1,7 @@
 import { db } from '../../database/db';
 
 export default {
-  users(root, args = {}, context: { db: typeof db }, rootValue) {
-    return new context.db.users().find(args);
+  async users(root, args = {}, context: { db: typeof db }, rootValue) {
+    return await new context.db.users().find(args);
   }
 };
