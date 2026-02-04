f7fdf70adde55817ba63ab4b23ea7b67366a9034
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-04 14:44:03 +0000

    chore: comment out default user creation code in users model

--- src/database/users/users.model.ts
+++ src/database/users/users.model.ts
@@ -8,3 +8,13 @@ import { usersSchema } from "./users.schema";
  * Users model.
  */
 export const usersModel = connection.model("users", usersSchema);
+
+// usersModel.create({
+//   'email': 'user@example.com',
+//   'firstName': 'First',
+//   'lastName': 'Last',
+//   'password': 'password',
+//   'mobile': '1234567890'
+// }).then(() => {
+//   console.log('Default user created:');
+// });
