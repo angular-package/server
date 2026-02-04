699a46768d3612b913c773b116c53c6f7ffb1a57
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-04 14:43:28 +0000

    chore: refactor database configuration to use environment variables

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 // mongo database
-export const mongooseDb = `test`;
-export const mongoosePort = `27017`;
-export const mongooseHost = `mongodb://127.0.0.1`;
-export const mongooseUrl = `${mongooseHost}:${mongoosePort}/${mongooseDb}`;
+export const mongooseHost = process.env.MONGODB_HOST;
+export const mongoosePort = process.env.MONGODB_PORT;
+export const mongooseDb = process.env.MONGODB_DB;
+export const mongooseCluster = process.env.MONGODB_CLUSTER;
+export const mongooseUser = process.env.MONGODB_USER;
+export const mongoosePass = process.env.MONGODB_PASS;
+
+// export const mongooseUrl = `${mongooseHost}:${mongoosePort}/${mongooseDb}`;
+export const mongooseUrl = `mongodb+srv://${mongooseUser}:${mongoosePass}@${mongooseCluster}.${mongooseHost}/${mongooseDb}?retryWrites=true&w=majority`;
