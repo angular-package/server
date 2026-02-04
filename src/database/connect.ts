8ad4943e8054006ba8943f78d7f80efa24aa2a65
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-04 14:43:46 +0000

    chore: refactor mongoose connection setup to improve readability and include additional logging

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
 
 // Get config
-import { mongooseDb, mongooseHost, mongoosePort, mongooseUrl } from "./config";
+import {
+  mongooseHost,
+  mongoosePort,
+  mongooseCluster,
+  mongooseDb,
+  mongooseUser,
+  mongooseUrl
+} from "./config";
+
+console.log('MONGODB_URI', mongooseUrl);
 
 // Create connection.
-export const connection = mongoose.createConnection(`${mongooseUrl}`, {
+export const connection = mongoose.createConnection(mongooseUrl, {
   keepAlive: true,
   socketTimeoutMS: 0,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 });
 
 connection.on("error", console.error.bind(console, "connection error:"));
@@ -17,7 +26,9 @@ connection.once("open", () => {
   console.log(`Mongo connection`)
   console.log(`- host: ${mongooseHost}`);
   console.log(`- port: ${mongoosePort}`);
+  console.log(`- cluster: ${mongooseCluster}`);
   console.log(`- db: ${mongooseDb}`);
+  console.log(`- user: ${mongooseUser}`);
   console.log(`opened on ${mongooseUrl}`);
   console.log(`--------------------------------------------------------------------------------`);
   // we're connected!
